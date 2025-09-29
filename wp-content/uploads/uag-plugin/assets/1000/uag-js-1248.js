57b396d96de81c9a2d751b24d955e3ae05716593
EscondidoBitcoinMeetup <escomeetup@sharefreely.com>
2025-09-29 16:31:21 -0700

    Updated Meetup Page links
    Linked to the Goli website...

--- wp-content/uploads/uag-plugin/assets/1000/uag-js-1248.js
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1248.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-04b70307' );
+});
+window.addEventListener( 'load', function() {
 	UAGBButtonChild.init( '.uagb-block-55ba2e4c' );
 });
  });
